71d3b5bbd772f649be25463b90ec110f40aa5660
Hoi.Kwon <teaho177@gmail.com>
2023-12-05 01:34:43 +0900

    feat: 시작재고 인스턴스 생성 및 재고현황 업데이트 함수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -8,5 +8,16 @@ import Foundation
 
 // 과일 저장소 타입
 class FruitStore {
-    
+    private var fruitInventory: [String: Int] = [
+        "딸기": 10,
+        "바나나": 10,
+        "파인애플": 10,
+        "키위": 10,
+        "망고": 10
+    ]
+    func updateFruitQuantity(fruit: String, quantity: Int) {
+        if let currentQuantity = fruitInventory[fruit] {
+            fruitInventory[fruit] = max(0, currentQuantity + quantity)
+        }
+    }
 }
