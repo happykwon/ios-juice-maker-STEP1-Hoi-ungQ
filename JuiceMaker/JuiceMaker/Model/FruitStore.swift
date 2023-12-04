ae1c91ff3f7467da5cc70525a025344b0afb5048
Hoi.Kwon <teaho177@gmail.com>
2023-12-05 01:45:07 +0900

    feat: JuiceMaker구조체 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -28,5 +28,19 @@ class FruitStore {
         }
         return true
     }
+    func makeJuice(type: JuiceRecipe) -> String {
+        var  ingredients = type.ingredients
+        for (fruit, quantity) in ingredients {
+            guard let available = fruitInventory[fruit], available >= quantity else {
+                return "재료가 부족하여 \(type.name)를 제조할 수 없습니다."
+            }
+        }
+        for (fruit, quantity) in ingredients {
+            if let available = fruitInventory[fruit], let currentQuantity = fruitInventory[fruit], available >= quantity {
+                fruitInventory[fruit] = currentQuantity - quantity
+            }
+        }
+        return "\(type.name)가 제조되었습니다."
+    }
     
 }
